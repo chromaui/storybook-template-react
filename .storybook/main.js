7367a5906dfac34f5d89394f46a0a7420fc2e091
Tom Coleman <tom@thesnail.org>
2020-06-03 16:52:24 +1000

    Add all essentials and introduction

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,12 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  stories: ['../src/**/*.stories.(js|mdx)'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
-    '@storybook/addon-links',
+    '@storybook/addon-docs',
+    '@storybook/addon-viewport',
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-controls',
+    '@storybook/addon-toolbars',
   ],
 };
