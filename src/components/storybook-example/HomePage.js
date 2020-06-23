fca8bee926ea34e506187583e97523719eb79803
Tom Coleman <tom@thesnail.org>
2020-06-23 16:16:27 +1000

    Build page

--- src/components/storybook-example/HomePage.js
+++ src/components/storybook-example/HomePage.js
@@ -7,14 +7,40 @@ export const HomePage = ({ user, onLogin, onLogout }) => (
   <article>
     <Header user={user} onLogin={onLogin} onLogout={onLogout} />
 
+    <h2>Pages in Storybook</h2>
+
     <section>
-      This page demonstrates that you can build full application screens using Storybook. Although
-      this page is simple and doesn't have any input data, screens typically either take data
-      gathered by a higher-level connected component (in stories you can compose such data from the
-      args of sub-component stories), or assemble data themselves using services which you can mock
-      out using Storybook. <br />
-      Read more in the docs and in Learn Storybook, a tutorial for building components with
-      Storybook (see links above).
+      We recommend building UIs with a{' '}
+      <a href="https://blog.hichroma.com/component-driven-development-ce1109d56c8e" target="_blank">
+        component-driven
+      </a>{' '}
+      process starting with atomic components and edning with pages. <br />
+      <br />
+      Render pages with mock data. This makes it easy to review page states without needing to
+      navigate to them in an app. Here are some handy patterns for managing page data in Storybook:
+      <ul>
+        <li>
+          Use a higher-level connected component. Storybook helps you compose such data from the
+          "args" of child component stories
+        </li>
+        <li>
+          Assemble data in the page component from your services. You can mock these services out
+          using Storybook.
+        </li>
+      </ul>
+      Get a guided tutorial on component-driven development at{' '}
+      <a href="https://www.learnstorybook.com" target="_blank">
+        Learn Storybook
+      </a>
+      . Read more in the{' '}
+      <a href="https://storybook.js.org/docs" target="_blank">
+        docs
+      </a>
+      .
+    </section>
+    <section>
+      <span class="tip">Tip</span> Adjust the width of the canvas with the <span />
+      Viewports addon in the toolbar
     </section>
   </article>
 );
