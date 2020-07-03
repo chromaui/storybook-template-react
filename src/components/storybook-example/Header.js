e743ff43839e1ebc4cbc1393638ab28302f9b441
domyen <dom@hichroma.com>
2020-07-03 17:03:12 -0400

    Style Header

--- src/components/storybook-example/Header.js
+++ src/components/storybook-example/Header.js
@@ -3,19 +3,67 @@ import PropTypes from 'prop-types';
 
 import { Button } from './Button';
 
+const css = `
+  .wrapper {
+    font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+    border-bottom: 1px solid rgba(0,0,0,.1);
+    padding: 15px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  
+  svg {
+    display: inline-block;
+    vertical-align: top;
+  }
+
+  h1 {
+    font-weight: 900;
+    font-size: 20px;
+    line-height: 1;
+    margin: 6px 0 6px 10px;
+    display: inline-block;
+    vertical-align: top;
+  }
+`;
+
 export const Header = ({ user, onLogin, onLogout, onCreateAccount }) =>
   console.log({ onLogin }) || (
-    <header style={{ borderBottom: '1px solid #eee', paddingBottom: '12px' }}>
-      <span className="icon" />
-      <h1>Acme</h1>
-      {user ? (
-        <Button size="medium" onClick={onLogout} label="Log out" />
-      ) : (
+    <header>
+      <style>{css}</style>
+      <div className="wrapper">
         <div>
-          <Button size="medium" onClick={onLogin} label="Log in" />
-          <Button primary size="medium" onClick={onCreateAccount} label="Sign up" />
+          <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+            <g fill="none" fillRule="evenodd">
+              <path
+                d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
+                fill="#FFF"
+              />
+              <path
+                d="M5.3 10.6l10.4 6v11.1l-10.4-6v-11zm11.4-6.2l9.7 5.5-9.7 5.6V4.4z"
+                fill="#555AB9"
+              />
+              <path
+                d="M27.2 10.6v11.2l-10.5 6V16.5l10.5-6zM15.7 4.4v11L6 10l9.7-5.5z"
+                fill="#91BAF8"
+              />
+            </g>
+          </svg>
+          <h1>Acme</h1>
         </div>
-      )}
+        <div>
+          {user ? (
+            <Button size="small" onClick={onLogout} label="Log out" />
+          ) : (
+            <>
+              <Button size="small" onClick={onLogin} label="Log in" />
+              <span style={{ display: 'inline-block', marginRight: 10 }}></span>
+              <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
+            </>
+          )}
+        </div>
+      </div>
     </header>
   );
 
